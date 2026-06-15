9d17141206b8168223b6e55dd9ad4a3cc1f136c4
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2026-06-15 03:17:10 -0300

    fix: adicionar URL do Vercel no CORS

--- backend/src/main/java/site/psi/ads3/config/SecurityConfig.java
+++ backend/src/main/java/site/psi/ads3/config/SecurityConfig.java
@@ -57,7 +57,8 @@ public class SecurityConfig {
             "http://127.0.0.1:5500",
             "http://localhost:5500",
             "http://127.0.0.1:5501",
-            "http://localhost:5501"
+            "http://localhost:5501",
+            "https://com-anonimos-jogadores-ads3.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of("*"));
